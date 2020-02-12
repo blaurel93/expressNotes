3a30a307c8439cf880b71c7fb5bef4e693caca1f
Blaise <ifollowjre@gmail.com>
2020-02-11 19:51:55 -0700

    re added folders trying to fix a bug

---
+++ README.md
@@ -0,0 +1,9 @@
+### Note taker with Express
+
+this application is used to store notes and retrieve them back when the user needs them. Also the user can delete them when ever and it all works with an express server.
+
+
+
+---
+##  License and Copyright 
+© Tom Ruff, U of U Full Stack Bootcamp
---
+++ routes/apiRoutes.js
@@ -0,0 +1,14 @@
+var noteData = require("../db/db.json");
+
+// console.log(noteData);
+
+module.exports = function (app) {
+    app.get("/api/notes", function (req, res) {
+        res.json(noteData);
+    });
+
+    app.post("/api/notes", function (req, res) {
+        noteData.push(req.body);
+        res.json(true);
+    });
+}
